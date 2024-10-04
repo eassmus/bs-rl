ef8727f7800e5cabec4b12b80b2bacafff6677b6
Pulsar25 <evanwassmus@gmail.com>
2024-10-03 20:37:18 -0400

    actually fixed it this time

--- aggressive_agent.py
+++ aggressive_agent.py
@@ -7,11 +7,14 @@ class AggressiveAgent(env.Agent):
 
     def get_card(self, intended_card, hand):
         if hand[intended_card] > 0:
-            return intended_card, hand.count(intended_card)
+            return intended_card, hand[intended_card]
         return random.choice([card for card in hand if hand[card] > 0]), 1
     
     def get_call_bs(self, player_index, card, card_amt, hand):
         return hand[card] > 0
 
     def give_info(self, player_indexes_picked_up):
+        pass
+
+    def reset(self):
         pass
