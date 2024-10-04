c590e086f00960d10353bfe57fada33be72cce97
blwright13 <158185925+blwright13@users.noreply.github.com>
2024-10-03 22:34:27 -0400

    Add files via upload

---
+++ human_agent.py
@@ -0,0 +1,41 @@
+import env
+
+cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+class HumanAgent(env.Agent):
+    def __init__(self, my_index, num_players, agent_args = []):
+        pass
+
+    def get_card(self, intended_card, hand) -> tuple[str, int]:
+        print("Your hand:", hand)
+        print("Intended card:", intended_card)
+        while True:
+            card = input("Choose card:")
+            if card in cards:
+                while True:
+                    num = int(input("Choose amount:"))
+                    if hand[card] >= num and num >= 0:
+                        return card, num
+                    else:
+                        print("Invalid amount")
+            else:
+                print("Invalid card")
+        
+
+    def get_call_bs(self, player_index, card, card_amt, hand) -> bool:
+        print(player_index, "played", card_amt, card)
+        print("Your hand:", hand)
+        while True:
+            response = input("Call BS (Y|N):")
+            if response == "Y":
+                return True
+            elif response == "N":
+                return False
+            else:
+                print("Invalid input")
+
+    def give_info(self, player_indexes_picked_up):
+        print("Player", player_indexes_picked_up, "picked up the pile")
+    
+    def reset(self):
+        pass
