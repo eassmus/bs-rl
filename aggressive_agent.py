798ecb119e1243192c0d6f89f8cf63f89ec07c1c
Pulsar25 <evanwassmus@gmail.com>
2024-10-03 20:35:35 -0400

    updated aggressive agent to dict hand strcture

--- aggressive_agent.py
+++ aggressive_agent.py
@@ -6,14 +6,12 @@ class AggressiveAgent(env.Agent):
         pass 
 
     def get_card(self, intended_card, hand):
-        if intended_card in hand:
+        if hand[intended_card] > 0:
             return intended_card, hand.count(intended_card)
-        return random.choice(hand), 1
+        return random.choice([card for card in hand if hand[card] > 0]), 1
     
     def get_call_bs(self, player_index, card, card_amt, hand):
-        if card in hand:
-            return True
-        return False
+        return hand[card] > 0
 
     def give_info(self, player_indexes_picked_up):
         pass
